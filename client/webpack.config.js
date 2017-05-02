059efefaf39d015c87edf01fcb7c17f8ca58a6be
Ross Crichton <rcrichton87@googlemail.com>
2017-05-02 16:04:07 +0100

    initial commit

---
+++ client/webpack.config.js
@@ -0,0 +1,10 @@
+var config = {
+  entry: __dirname + '/src/index.js',
+  output: {
+    filename: "bundle.js",
+    path: __dirname + '/build'
+  },
+  devtool: "source-map"
+}
+
+module.exports = config;
